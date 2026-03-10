e5b14c7029c03900d0f1c90413e768e90ab218ea
mehul79 <mehulrgupta@gmail.com>
2026-03-10 13:24:17 +0530

    base setup done

--- be/src/lib/auth.ts
+++ be/src/lib/auth.ts
@@ -1,12 +1,13 @@
+import "dotenv/config";
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
-// If your Prisma file is located elsewhere, you can change the path
-import { PrismaClient } from "@/generated/prisma/client";
+import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
+
 export const auth = betterAuth({
     database: prismaAdapter(prisma, {
-        provider: "sqlite", // or "mysql", "postgresql", ...etc
+        provider: "postgresql", // or "mysql", "postgresql", ...etc
     }),
     socialProviders: { 
       github: { 
